42909e1a60372ccf817b7890bb92d9ec21c1d41d
jason <jason.m.doyle1@gmail.com>
2023-04-18 20:16:42 -0400

    final push

--- user-data.sh
+++ user-data.sh
@@ -4,7 +4,7 @@ yum update -y
 yum install httpd -y
 yum install mod_ssl -y
 aws s3 cp s3://bboys-jd-test/index.html /var/www/html/index.html
-aws s3 cp s3://bboys-jd-test/httpd.conf /etc/httpd/conf/httpd.conf
+#aws s3 cp s3://bboys-jd-test/httpd.conf /etc/httpd/conf/httpd.conf
 aws s3 cp s3://bboys-jd-test/ssl.conf /etc/httpd/conf.d/ssl.conf
 aws s3 cp s3://bboys-jd-test/localhost.crt /etc/pki/tls/certs
 systemctl start httpd
